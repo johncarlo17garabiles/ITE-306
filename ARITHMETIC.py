d6a43359abd3ff57620d747b47012e8eef6ef23f
johncarlo17garabiles <114630003+johncarlo17garabiles@users.noreply.github.com>
2023-07-05 23:52:04 -0700

    Add files via upload

---
+++ ARITHMETIC.py
@@ -0,0 +1,14 @@
+num1 = 5
+num2 = 8
+
+sum = num1 + num2
+print("SUM = {0}".format(sum))
+
+diff = num1 - num2
+print("Difference = {0}".format(diff))
+
+product = num1 * num2
+print("Product = {0}".format(product))
+
+qout = num1 / num2
+print("Qoutient = {0}".format(qout))
