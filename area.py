707ead2733db1ba353731a73deb6a99f57431b1f
johncarlo17garabiles <114630003+johncarlo17garabiles@users.noreply.github.com>
2023-07-08 09:14:03 +0800

    Add files via upload

---
+++ area.py
@@ -0,0 +1,7 @@
+print("Enter value for length: ")
+length = int(input())
+print("Enter value for width: ")
+width = int(input())
+area = length * width
+
+print("length ({0}) * width({1}) = area ({2})".format(length, width, area))
